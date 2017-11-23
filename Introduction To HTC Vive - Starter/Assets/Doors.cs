712794697007c3b811b4c10c44a99b7afbba90e1
yousfiy <yousfiy>
2017-11-23 10:58:20 +0100

    Door trigger

---
+++ Introduction To HTC Vive - Starter/Assets/Doors.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour {
+
+    // Use this for initialization
+    Animator animator;
+    bool doorOpen;
+
+    void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            doorOpen = true;
+            DoorDirection("Open");
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            DoorDirection("Close");
+        }
+    }
+
+    void DoorDirection(string direction)
+    {
+        animator.SetTrigger(direction);
+    }
+}
