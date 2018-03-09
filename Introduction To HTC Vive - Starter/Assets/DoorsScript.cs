21f24e14580c3da993af57141fb4b09cf4ac934a
Maxime Pichaud <maxime.pichaud@edu.esiee.fr>
2018-03-09 12:02:07 +0100

    Door Animation

---
+++ Introduction To HTC Vive - Starter/Assets/DoorsScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorsScript : MonoBehaviour {
+
+    Animator anim;
+    
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool won;
+        won = true;
+        anim.SetBool("won", won);
+	}
+}
