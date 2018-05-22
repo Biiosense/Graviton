0c4b4d6ab87ea3410f496379db6b541a5ab55457
Thibaud Bouriez <thibaud.bouriez@edu.esiee.fr>
2018-05-22 14:27:36 +0200

    Fin modelisation salle 4 haut

---
+++ Introduction To HTC Vive - Starter/Assets/Scripts/ColorPickerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ColorPickerScript : MonoBehaviour {
+
+    private string[] color_names = new string[] { "white", "blue", "cyan", "grey", "orange", "purple", "black" };
+    public MonoBehaviour capsule;
+    private int color_id = 1;
+    Color color;
+    new Renderer renderer;
+    float last_time_pressed;
+    VRTK_InteractableObject eventScript;
+
+    // Use this for initialization
+    void Start () {
+        renderer = GetComponent<Renderer>();
+        ColorUtility.TryParseHtmlString(color_names[color_id], out color);
+        last_time_pressed = 0.0f;
+        eventScript = GetComponent(typeof(VRTK_InteractableObject)) as VRTK_InteractableObject;
+        eventScript.InteractableObjectTouched += handleEvent;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ColorUtility.TryParseHtmlString(color_names[color_id], out color);
+        renderer.material.color = color;
+        if (last_time_pressed > 0.0f) 
+            last_time_pressed -= Time.deltaTime;
+
+    }
+
+    void handleEvent(object sender, InteractableObjectEventArgs e)
+    {
+        if (last_time_pressed <= 0.0f)
+        {
+            last_time_pressed = 0.5f;
+            color_id++;
+            color_id = color_id % color_names.Length;
+        }
+    }
+
+    /*
+    public bool getConditionStatus()
+    {
+        Renderer capsuleRenderer = capsule.GetComponent<Renderer>();
+        return capsuleRenderer.material.color == renderer.material.color;
+    }*/
+
+}
